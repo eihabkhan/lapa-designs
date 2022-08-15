e794b7c7b1385e1da660d9063d612dbb2c0de1f7
Eihab Khan <mastereihab@gmail.com>
2022-08-15 20:07:02 +0100

    initialize nextjs project and add tailwind

---
+++ idle-finance/src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline text-purple-500">
+                Hello world!
+            </h1>
+        </div>
+    );
+}
